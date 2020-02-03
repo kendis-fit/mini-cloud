7330b7c1d5b5fa7df700c029b0d2ce0e585eac08
Kurt Endless <kendis_it@hotmail.com>
2020-02-03 15:06:00 +0200

    add: method store by id

--- src/routes/storeRoute.js
+++ src/routes/storeRoute.js
@@ -1,8 +1,28 @@
 const express = require("express");
 
+const item = require("../models/item").default;
+const store = require("../models/store").default;
+
 const storeRoute = express.Router();
 
-storeRoute.get("/:id");
+storeRoute.get("/:id", async (req, res) => {
+    try
+    {
+        const id = req.params.id;
+        const projection = { _id: 0 };
+        const storeDTO = await store.findOne({ _id: id }, projection).lean().exec();
+        if (!storeDTO.items.length === 0) {
+            res.sendStatus(404);
+        } else {
+            res.status(200).send(storeDTO.items);
+        }
+    }
+    catch
+    {
+        res.sendStatus(500);
+    }
+});
+
 storeRoute.post("/")
 storeRoute.get("/:id/");
 storeRoute.post("/:id/items");
